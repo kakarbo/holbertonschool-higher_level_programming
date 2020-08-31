2e72d702295838f1f49e3b3c0037654567f99aa7
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-08-31 23:58:18 +0000

    Copy-cut-paste

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word
+word_last_2 = word
+middle_word = word
+print("Fist 3 letters: {}".format(word_first_3[0:3]))
+print("Last 2 letters: {}".format(word_last_2[7:9]))
+print("Middle word: {}".format(middle_word[1:8]))
