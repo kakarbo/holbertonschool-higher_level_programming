353ed7f6d72031cd7d6b9bd2f8d4fe5f7c332a46
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-01 00:24:11 +0000

    Copy-cut-paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = "Hol"
-word_last_2 = "on"
-middle_word = "olberto"
+word_first_3 = word[0:3]
 print("First 3 letters: {}".format(word_first_3))
+word_last_2 = word[7:9]
 print("Last 2 letters: {}".format(word_last_2))
+middle_word = word[1:8]
 print("Middle word: {}".format(middle_word))
