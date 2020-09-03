a5a958a7754af05b96ddd70a08aa6041c3244b29
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-03 22:01:38 +0000

    My firts toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-#if __name__ == "__main__":
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-str = "{} + {} = {}"
-print(str.format(a, b, add(a, b)))
-print(str.format(a, b, sub(a, b)))
-print(str.format(a, b, mul(a, b)))
-print(str.format(a, b, div(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    d = '+'
+    c = '='
+    str = ("{} {} {} {} {}")
+    print(str.format(a, d, b, c, add(a, b)))
+    print(str.format(a, d, b, c, sub(a, b)))
+    print(str.format(a, d, b, c, mul(a, b)))
+    print(str.format(a, d, b, c, div(a, b)))
