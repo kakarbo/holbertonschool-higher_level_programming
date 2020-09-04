b8e92cf7c58f6f1862aecfa0770b9f8fc547cea8
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-04 22:10:07 +0000

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,21 +3,20 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     argv = sys.argv
-    conteo = len(sys.argv) - 1
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator <a> <operator> <b>")
         exit(1)
-    a = argv[conteo - 2]
-    b = argv[conteo]
-    operato = argv[conteo - 1]
+    a = argv[1]
+    b = argv[3]
+    operato = argv[2]
     if "+" == operato:
-        print("{} {} {} = {}".format(a, operato, b, add(int(a), int(b))))
+        print("{} + {} = {}".format(a, operato, b, add(int(a), int(b))))
     elif "-" == operato:
-        print("{} {} {} = {}".format(a, operato, b, sub(int(a), int(b))))
+        print("{} - {} = {}".format(a, operato, b, sub(int(a), int(b))))
     elif "*" == operato:
         print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
     elif '/' == operato:
-        print("{} {} {} = {}".format(a, operato, b, div(int(a), int(b))))
+        print("{} / {} = {}".format(a, operato, b, div(int(a), int(b))))
     else:
         print("Unknow operator. Available operators: +, -, * and /")
         exit(1)
