c5d9fc50485b4d8b37896db6ebd700c866a75618
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-04 23:44:04 +0000

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,18 +5,18 @@ if __name__ == "__main__":
     argv = sys.argv
     if len(argv) != 4:
         print("Usage: ./100-my_calculator <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
     a = argv[1]
     b = argv[3]
     operato = argv[2]
     if "+" == operato:
-        print("{} {} {} = {}".format(a, operato, b, add(a, b)))
+        print("{} {} {} = {}".format(a, operato, b, add(int(a), int(b))))
     elif "-" == operato:
-        print("{} {} {} = {}".format(a, operato, b, sub(a, b)))
+        print("{} {} {} = {}".format(a, operato, b, sub(int(a), int(b))))
     elif "*" == operato:
-        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
     elif '/' == operato:
-        print("{} {} {} = {}".format(a, ,operato b, div(a, b)))
+        print("{} {} {} = {}".format(a, operato, b, div(int(a), int(b))))
     else:
         print("Unknow operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
