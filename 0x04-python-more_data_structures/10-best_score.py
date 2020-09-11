9bdb07813c031395ac71d84556835d415eea1538
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-11 01:57:36 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return(None)
-        '''for a, b in a_dictionary.items():
-            max_key = max(a_dictionary)
-            if a == max_key:
-                return(max_key)'''
     else:
-        return(max(a_dictionary))
+        return max(a_dictionary)
