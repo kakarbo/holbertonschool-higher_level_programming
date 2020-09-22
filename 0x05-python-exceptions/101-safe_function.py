e6552b0b51c63b3a56a1d7870678643f72ce07ee
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-22 04:57:10 +0000

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    stderr_fileno = sys.stderr
+    result = 0
+    try:
+        resutl = fct(args[0], args[1])
+    except BaseException as err:
+        print("Exception: {}".format(err), file=stderr_fileno)
+        return(None)
+    return(result)
