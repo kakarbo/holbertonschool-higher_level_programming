d2533763f4517650268e1c28fbf699c0119519dd
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-23 20:58:33 +0000

    Corrdinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,14 +40,11 @@ with the message size must be >= 0"""
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
-            print("position must be a tuple of 2 positive integers")
-            raise TypeError
+            raise TypeError("position must be a tuple of 2 positive integers")
         if not isinstance(value[0], int) or not isinstance(value[1], int):
-            print("position must be a tuple of 2 positive integers")
-            raise TypeError
+            raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
-            print("position must be a tuple of 2 positive integers")
-            raise TypeError
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
