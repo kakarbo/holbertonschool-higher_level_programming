0ad4896a83d705b34947cf7c67eb4547a19720ba
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-06 23:52:58 +0000

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,7 @@
 
 class MyList(list):
     """subclass inheriting from another class or object"""
+    
     def print_sorted(self):
         """Public instance method: def print_sorted(self): that prints
             the list, but sorted (ascending sort)"""
