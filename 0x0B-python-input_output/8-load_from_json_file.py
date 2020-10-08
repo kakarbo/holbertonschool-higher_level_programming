e2bf396bb0ce7cb91d9b07b8fd2e36f782f11299
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-08 03:30:39 +0000

    Create object from a JSON file

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     ou don’t need to manage exceptions if the JSON string doesn’t
     represent an object.
     """
-    with open(filename, mode="w", encoding="utf-8") as myFile:
+    with open(filename, mode="r") as myFile:
         return(json.load(myFile))
