e3de0119484620ccbd4d17d46cef9373fe67b2ac
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-08 00:54:42 +0000

    To JSON string

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -3,9 +3,9 @@
 function that returns the JSON
 representation of an object (string):
 """
-
-
 import json
+
+
 def to_json_string(my_obj):
     """
     You don’t need to manage exceptions if the object can’t be serialized.
