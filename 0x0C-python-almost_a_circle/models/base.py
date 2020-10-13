f5dbae243d49b029f49aeee6ed29876c1e75e620
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-13 14:49:08 +0000

    First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class Base
+"""
+
+
+class Base:
+    """
+    Class Base
+    """
+    __nb_objects = 0  # private class attribute
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
