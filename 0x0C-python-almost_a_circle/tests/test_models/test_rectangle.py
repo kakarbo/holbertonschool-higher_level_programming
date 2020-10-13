1d1fb68fd2d25edab3dc8b63ebe4e5a076733418
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-13 21:25:00 +0000

    test rectangle update n. °1

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -29,11 +29,25 @@ class TestRectangle(unittest.TestCase):
         """
         self.assertEqual(self.test.width, 6)
 
+    def test_get_set_width(self):
+        """
+        test width getter/setter
+        """
+        self.test.width = 9
+        self.assertEqual(self.test.width, 9)
+
     def test_height(self):
         """
         test height
         """
         self.assertEqual(self.test.height, 6)
+
+    def test_get_set_height(self):
+        """
+        test height getter/setter
+        """
+        self.test.height = 9
+        self.assertEqual(self.test.height, 9)
 
     def test_x(self):
         """
@@ -41,10 +55,89 @@ class TestRectangle(unittest.TestCase):
         """
         self.assertEqual(self.test.x, 6)
 
+    def test_get_set_x(self):
+        """
+        test x getter/setter
+        """
+        self.test.x = 9
+        self.assertEqual(self.test.x, 9)
+
     def test_y(self):
         """
         test y
         """
+        self.assertEqual(self.test.y, 6)
+
+    def test_get_set_y(self):
+        """
+        test y getter/setter
+        """
+        self.test.y = 9
+        self.assertEqual(self.test.y, 9)
+
+    def testArea(self):
+        """
+        test area
+        """
+        self.assertEqual(self.test.area(), 36)
+
+    def testDisplay(self):
+        """
+        test display
+        """
+        self.assertIsNone(self.test.display())
+
+    def testNoArgs(self):
+        """
+        test no args
+        """
+        with self.assertRaises(TypeError):
+            empty = Rectangle()
+
+    def testUpdateArgs(self):
+        """
+        test update *args
+        """ 
+        self.test.update(8,8,8,8,8)
+        self.assertEqual(self.test.id, 8)
+        self.assertEqual(self.test.width, 8)
+        self.assertEqual(self.test.height, 8)
+        self.assertEqual(self.test.x, 8)
+        self.assertEqual(self.test.y, 8)
+
+    def testUpdateKwargsId(self):
+        """
+        test update **kwargs id
+        """
+        self.test.update(id=6)
+        self.assertEqual(self.test.id, 6)
+
+    def testUpdateKwargsWidht(self):
+        """
+        test update **kwargs width
+        """
+        self.test.update(width=6)
+        self.assertEqual(self.test.width, 6)
+
+    def testUpdateKwargsHeight(self):
+        """
+        test update **kwargs height
+        """
+        self.test.update(height=6)
+        self.assertEqual(self.test.height, 6)
+
+    def testUpdateKwargs_x(self):
+        """
+        test update **kwargs x
+        """
+        self.test.update(x=6)
+        self.assertEqual(self.test.x, 6)
+
+    def testUpdateKwargs_y(self):
+        """
+        test update **kwargs y
+        """
+        self.test.update(y=6)
         self.assertEqual(self.test.y, 6)
 
 if __name__ == "__main__":
