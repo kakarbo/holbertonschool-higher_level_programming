aad377f15b227c869ac3a4a64b46afc1af3f6b37
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-13 21:34:33 +0000

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,7 +9,11 @@ class Rectangle(Base):
     """
     Class Rectangle that inherits from Base:
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        the class Rectangle that inherits from Base:
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -18,10 +22,18 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         return(self.__width)
 
     @width.setter
     def width(self, value):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -31,10 +43,18 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         return(self.__height)
 
     @height.setter
     def height(self, value):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -44,10 +64,18 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         return(self.__x)
 
     @x.setter
     def x(self, value):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         if type(value) is not int:
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -57,10 +85,18 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         return(self.__y)
 
     @y.setter
     def y(self, value):
+        """
+        the class Rectangle by adding validation of
+        all setter methods and instantiation (id excluded):
+        """
         if type(value) is not int:
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -69,41 +105,60 @@ class Rectangle(Base):
             self.__y = value
 
     def area(self):
+        """
+        the class Rectangle by adding the public method def
+        area(self): that returns the area value of the
+        Rectangle instance.
+        """
         return(self.__width * self.__height)
 
-    #def display(self):
-        #str1 = "".join(" " * self.__x)
-        #str2 = "".join("#" * self.__width)
-        #for index in range(self.__y):
-            #print()
-        #for index in range(self.__height):
-            #print(str1 + str2)
+    def display(self):
+        """
+        class Rectangle by adding the public method def
+        display(self): that prints in stdout the Rectangle
+        instance with the character
+        # - you don’t need to handle x and y here.
+        """
+        str1 = "".join(" " * self.__x)
+        str2 = "".join("#" * self.__width)
+        for index in range(self.__y):
+            print()
+        for index in range(self.__height):
+            print(str1 + str2)
 
-    #def __str__(self):
-        #str1 = "[{}] ({}) ".format(Rectangle.__name__, self.id)
-        #str2 = "{}/{} - ".format(self.__x, self.__y)
-        #return(str1 + str2 + "{}/{}".format(self.__width, self.__height))
+    def __str__(self):
+        """
+        the class Rectangle by overriding
+        the __str__ method so that it returns
+        """
+        str1 = "[{}] ({}) ".format(Rectangle.__name__, self.id)
+        str2 = "{}/{} - ".format(self.__x, self.__y)
+        return(str1 + str2 + "{}/{}".format(self.__width, self.__height))
 
-    #def update(self, *args, **kwargs):
-        #if len(args) > 0:
-            #self.id = args[0]
-        #if len(args) > 1:
-            #self.__width = args[1]
-        #if len(args) > 2:
-            #self.__height = args[2]
-        #if len(args) > 3:
-            #self.__x = args[3]
-        #if len(args) > 4:
-            #self.__y = args[4]
-        #if args is None or len(args) == 0:
-            #for key, value in kwargs.items():
-                #if key == "id":
-                    #self.id = value
-                #if key == "width":
-                    #self.__width = value
-                #if key == "height":
-                    #self.__height = value
-                #if key == "x":
-                    #self.__x = value
-                #if key == "y":
-                    #self.__y = value
+    def update(self, *args, **kwargs):
+        """
+        the class Rectangle by improving the public
+        method def display(self):
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.__width = args[1]
+        if len(args) > 2:
+            self.__height = args[2]
+        if len(args) > 3:
+            self.__x = args[3]
+        if len(args) > 4:
+            self.__y = args[4]
+        if args is None or len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
