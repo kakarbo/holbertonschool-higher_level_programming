135de5b44c66e3b56f1162929aa1430c6209d466
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-14 02:22:50 +0000

    Contains_

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State).order_by(State.id):
-    if "a" in state.name:
-        print("{}: {}".format(state.id, state.name))
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
