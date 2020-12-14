d88417dc26f3d46e23ac6463861cb8cea9df1d9e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-14 04:26:01 +0000

    Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """
 Python file similar to model_state.py named model_city.py that
 contains the class definition of a City.
 """
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
 
 
 class City(Base):
