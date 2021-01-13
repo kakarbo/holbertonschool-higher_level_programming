557bb19f889f9180fc7c4d76b172df13bd9b03b5
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-13 15:54:11 +0000

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+'''
+from requests import get
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[1], argv[2])
+    r = get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(commits[i].get("sha"), commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
