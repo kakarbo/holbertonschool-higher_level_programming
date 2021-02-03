d9edf1218c7be22bdff13bc56a94467524a9109e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-03 01:39:11 +0000

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-/*
-script that prints a message depending
-of the number of arguments passed
-*/
-if (process.argv.length == 3) {
+// script that prints a message depending
+// of the number of arguments passed
+
+if (process.argv.length === 3) {
 	console.log('Argument found');
 } else if (process.argv.length >= 4) {
 	console.log('Arguments found');
