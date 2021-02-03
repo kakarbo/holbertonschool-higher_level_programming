062ace619c4882234f98e048947e640970283beb
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-02 23:44:57 -0500

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // Script that prints a square
 const numberArgument = Number.parseInt(process.argv[2], 10);
-let myStrig = 'X'
+let myStrig = 'X';
 if (Number.isNaN(numberArgument)) {
   console.log('Missing size');
 } else {
-  for (let i = 1; i < numberArgument; i++){
+  for (let i = 1; i < numberArgument; i++) {
     myStrig += 'X';
   }
   for (let i = 1; i <= numberArgument; i++) {
     for (let j = i; j <= i; j++) {
-		console.log(myStrig);
+      console.log(myStrig);
     }
   }
 }
