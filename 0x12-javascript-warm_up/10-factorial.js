89bbcab9fc19374aa9a6feae406ffa674a0a4c24
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-03 10:37:44 -0500

    Función que haya el numero factorial de n de forma recursiva

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 function factorial (n) {
   let total = 0;
   if (n === 0 || process.argv.length <= 2) {
-    return 1
+    return 1;
   } else {
     total = n * factorial(n - 1);
   }
   return total;
 }
-factor = factorial(Number.parseInt(process.argv[2], 10));
+const factor = factorial(Number.parseInt(process.argv[2], 10));
 console.log(factor);
