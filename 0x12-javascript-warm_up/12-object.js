82fd5300acb102a1b455f910cb00e93254b9836e
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-03 15:13:36 -0500

    Object

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Script to replace the value 12 with 89
 const myObject = {
-	type: 'object',
-	value: 12
-  };
-  console.log(myObject);
-  for (const prop in myObject) {
-	  myObject[prop] = 89;
-  }
-  console.log(myObject);
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+for (const prop in myObject) {
+  myObject[prop] = 89;
+}
+console.log(myObject);
