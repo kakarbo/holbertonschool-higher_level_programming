18a8000fbc0262439ca12c6d4120d4904662143b
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-02 22:46:24 -0500

    joining two chains

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-/*
-Script that prints My number: <first argument converted in integer>
-if the first argument can be converted to an integer
-*/
-const number = Number.parseInt(process.argv[2], 10);
-if (Number.isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + number);
-}
+// Script that prints two arguments passed to it, in the following format: "is"
+console.log(process.argv[2] + ' is ' + process.argv[3]);
