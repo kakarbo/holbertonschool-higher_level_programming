317e4a253cddf55bb8763a9e8918180e6c9783d9
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-02 22:53:44 -0500

    adding data to an array and looping through forEach

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+Script that prints 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop
+*/
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+myArray.forEach(element => console.log(element));
