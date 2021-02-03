74f33f3f2e5246fc151f9441717605a47ff26f16
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-03 01:42:01 +0000

    update 2-arguments.js file documentation

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-// script that prints a message depending
-// of the number of arguments passed
+/*
+script that prints a message depending
+of the number of arguments passed
+*/
 
 if (process.argv.length === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else if (process.argv.length >= 4) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 } else {
-	console.log('No argument');
+  console.log('No argument');
 }
