5488a13a8652b892c594b830e89f3e4eff0d17c0
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-03 14:45:02 -0500

    Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Script that searches the second biggest integer in the list of arguments
+if (process.argv.length <= 2 || process.argv[2] === '1') {
+  console.log('0');
+} else {
+  const myArray = [0];
+  for (let i = 2; i < process.argv.length; i++) {
+    myArray.push(Number.parseInt(process.argv[i], 10));
+  }
+  const numberFirst = Math.max(...myArray);
+  const pos = myArray.indexOf(numberFirst);
+  for (let i = 0; i < myArray.length; i++) {
+    if (numberFirst === myArray[i]) {
+      myArray.splice(pos, 1);
+    }
+  }
+  console.log(Math.max(...myArray));
+}
