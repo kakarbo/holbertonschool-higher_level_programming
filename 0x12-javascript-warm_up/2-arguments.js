6bf836db82306da2c0ef659961b3f2fd44ebfd20
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-03 01:23:08 +0000

    Handling of arguments in javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+script that prints a message depending
+of the number of arguments passed
+*/
+if (process.argv.length >= 3) {
+	console.log('Argument found');
+} else {
+	console.log('No argument');
+}
