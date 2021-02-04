1f0e1d2ead8b3590c93ef77076321558957168a7
Cristhian Carbonell <alexcarbo8809@gmail.com>
2021-02-03 19:04:03 -0500

    Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -2,6 +2,6 @@
 // Function that executes x times a function
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
-    console.log('C is fun');
+    theFunction();
   }
 };
