16e2f4cb846c716dd1e065387bbb6f7b8400b264
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-08 21:25:44 -0500

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,9 +3,13 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
+      /* eslint-disable no-unused-vars */
       const person = {};
+      /* eslint-disable no-unused-vars */
     } else if (w === undefined || h === undefined) {
+      /* eslint-disable no-unused-vars */
       const person = {};
+      /* eslint-disable no-unused-vars */
     } else {
       this.width = w;
       this.height = h;
