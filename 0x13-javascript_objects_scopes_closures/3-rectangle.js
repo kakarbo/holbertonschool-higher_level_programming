73efb4ef89563cf74db32c6179e5bd92c0facc19
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-08 22:05:19 -0500

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      /* eslint-disable no-unused-vars */
+      const person = {};
+      /* eslint-disable no-unused-vars */
+    } else if (w === undefined || h === undefined) {
+      /* eslint-disable no-unused-vars */
+      const person = {};
+      /* eslint-disable no-unused-vars */
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  // method
+  print () {
+    let myString = 'X';
+    for (let i = 1; i < this.width; i++) {
+      myString += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < i; j++) {
+        console.log(myString);
+      }
+    }
+  }
+};
