44e6f910e51811ecf80f67881a749307399647e1
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-09 19:44:41 -0500

    Status Code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 // Script that display the status code of a GET request
 const request = require('request');
-request(process.argv[2], function (response) {
-  console.log('code: ', response && response.statusCode);
-});
+request
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log('code:', response.statusCode)
+  });
