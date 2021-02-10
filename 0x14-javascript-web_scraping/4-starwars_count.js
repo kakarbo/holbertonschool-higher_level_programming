b1b1660970bd45b098c0d4dd16453a358a88fd11
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-09 22:50:06 -0500

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,6 @@ Script that prints the number of movies where the character
 "Wedge Antilles" is present
 */
 const request = require('request');
-const peopleURL = 'https://swapi-api.hbtn.io/api/people/18/';
 request(process.argv[2], function (err, resp, body) {
   if (resp.statusCode === 404) {
     console.log(err);
@@ -12,10 +11,13 @@ request(process.argv[2], function (err, resp, body) {
   const json = JSON.parse(body);
   let i = 0;
   let count = 0;
-  for (const index in json) {
-    const people = json["results"][i++]["characters"];
+  for (let index in json) {
+    /* eslint-disable no-unused-vars */
+    index++;
+    /* eslint-disable no-unused-vars */
+    const people = json.results[i++].characters;
     for (const index of people) {
-      if (index === peopleURL) {
+      if (index.endsWith('18/') === true) {
         count += 1;
       }
     }
