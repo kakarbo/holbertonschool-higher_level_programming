d2954841c3f9ebb845fd7ecf8e0c07c0404289f7
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-02-10 11:33:18 -0500

    How many completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,21 +5,13 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  const json = JSON.parse(body);
-  let count = 0;
-  let num = 1;
   const obj = {};
-  for (const index in json) {
-    for (let i = 0; i <= 10; i++) {
-      if (json[index].userId === i) {
-        if (i !== num) {
-          count = 0;
-          num++;
-        }
-        if (json[index].completed === true) {
-          count += 1;
-        }
-        obj[i] = count;
+  for (const json of JSON.parse(body)) {
+    if (json.completed) {
+      if (obj[json.userId]) {
+        obj[json.userId] += 1;
+      } else {
+        obj[json.userId] = 1;
       }
     }
   }
